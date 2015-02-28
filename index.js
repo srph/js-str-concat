c2e75ddf534cddd29b2a53f5bc998353fe32a8fb
Kier Borromeo <seraphipod@gmail.com>
2015-02-28 21:25:12 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * Conditionally concatenates the provided strings
+ *
+ * @param {object} string Strings to be concatenated. Must be
+ * must be in form { "yolo-class": true, "awesome-class": false }
+ *
+ * @return string The concatenated string
+ */
+module.exports = concat = function concat(strings) {
+  // Let's first check of the provided `strings` is not an object
+  if ( typeof strings !== 'object') {
+    throw new Error(
+      'Strings to be concatenated must be an object, ' +
+    	'in form of { "yolo": true }'
+  	);
+  }
+
+  var keys = Object.keys(strings); // Keys of the object
+  var result = ''; // Our resulting string, where each string will be concatenated
+  var i = 0; // Used to check if we should add a space before the string is concatenated
+
+  for(let key of keys) {
+    // We'll concatenate if the key is true is `true`
+  	if ( !!strings[key] )
+      result += (i == 0 ? '' : ' ') + key;
+
+    ++i;
+  }
+
+  return result;
+}
