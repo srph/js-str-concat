3d2004c3757bf575babdd9be84bfef496f5cac0a
Kier Borromeo <seraphipod@gmail.com>
2015-03-01 19:33:16 +0800

    Updated to UMD
    https://github.com/umdjs/umd/blob/master/returnExports.js

--- index.js
+++ index.js
@@ -6,23 +6,38 @@
  *
  * @return string The concatenated string
  */
-module.exports = function concat(strings) {
-  // Let's first check if the provided `strings` is not an object
-  if ( typeof strings !== 'object') {
-    throw new Error(
-      'Strings to be concatenated must be an object, ' +
-      'in form of { "yolo": true }'
-    );
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.strConcat = factory();
   }
-
-  var keys = Object.keys(strings); // Keys of the object
-  var result = ''; // Our resulting string, where each string will be concatenated
+}(this, function () {
+  return function strConcat(strings) {
+    // Let's first check if the provided `strings` is not an object
+    if ( typeof strings !== 'object') {
+      throw new Error(
+        'Strings to be concatenated must be an object, ' +
+        'in form of { "yolo": true }'
+      );
+    }
   
-  keys.forEach(function(key) {
-    // We'll concatenate if the value of key (`{yolo: true}`) is `true`
-    if ( strings[key] === true )
-      result += (result == '' ? '' : ' ') + key;
-  });
-
-  return result;
-}
+    var keys = Object.keys(strings); // Keys of the object
+    var result = ''; // Our resulting string, where each string will be concatenated
+    
+    keys.forEach(function(key) {
+      // We'll concatenate if the value of key (`{yolo: true}`) is `true`
+      if ( strings[key] === true )
+        result += (result == '' ? '' : ' ') + key;
+    });
+  
+    return result;
+  }
+}));
